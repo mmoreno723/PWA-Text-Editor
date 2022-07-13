214880f90c88c3136ae7705f6146ef6c056250cf
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-07-12 21:38:24 -0400

    webpack config updated with src-sw inject manifest info

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,7 +22,10 @@ module.exports = () => {
         template: "./index.html",
         title: "Text Editor",
       }),
-      new InjectManifest(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
       new WebpackPwaManifest({
         name: "Text Editor",
         short_name: "Text Editor",
@@ -31,10 +34,13 @@ module.exports = () => {
         theme_color: "#7eb4e2",
         start_url: "./",
         publicPath: "./",
-        icons: {
-          src: path.resolve("src/images/logo.png"),
-          sizes: [96, 128, 192, 256, 384, 512],
-        },
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
